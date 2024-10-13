ddb050f5b40ecb29675d83db92ff6ed92cd55dd0
sunag <sunagbrasil@gmail.com>
2024-10-13 07:03:38 -0300

    update

--- examples/jsm/tsl/lighting/TiledLightsNode.js
+++ examples/jsm/tsl/lighting/TiledLightsNode.js
@@ -269,7 +269,7 @@ class TiledLightsNode extends THREE.LightsNode {
 
 	updateProgram( renderer ) {
 
-		renderer.getDrawingBufferSize( _size );
+		renderer.getSize( _size ); // ignore pixel ratio!
 
 		const width = this.getPowerOfTwo( _size.width );
 		const height = this.getPowerOfTwo( _size.height );
