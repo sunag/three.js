86f29c5acd6493e91c0ffe8c96c889b8c302835e
sunag <sunagbrasil@gmail.com>
2026-03-18 01:11:53 -0300

    Added `MyExtension`

---
+++ examples/jsm/inspector/addons/MyExtension.js
@@ -0,0 +1,13 @@
+import { Extension } from 'three/addons/inspector/Extension.js';
+
+class MyExtension extends Extension {
+
+	constructor() {
+
+		super( 'My Extension' );
+
+	}
+
+}
+
+export default MyExtension;
